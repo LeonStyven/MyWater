c446f86de8af7f15bf3d7388f5452e3e1d62c588
Leon Styven <j.stca03@gmail.com>
2023-10-15 22:09:10 -0500

    Navegation implemented

--- app/src/main/java/com/leonstyven/mywater/HomeActivity.java
+++ app/src/main/java/com/leonstyven/mywater/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.leonstyven.mywater;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        setupNavegacion();
+    }
+
+    private void setupNavegacion(){
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_hostfragment);
+        NavigationUI.setupWithNavController(
+                bottomNavigationView,
+                navHostFragment.getNavController()
+        );
     }
 }
